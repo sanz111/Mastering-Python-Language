1d975294c6cce148f04b2c103eeaadef6bb681cf
fir0j <firoj.is.availble@gmail.com>
2019-09-23 15:33:16 +0530

    inheritance concept in python

---
+++ 19.inheritance.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+#METHOD 1
+# Using parent_class_name instead of parent_class_instance for accessing instance variable of parent class
+
+class parent1:
+
+    a = 20
+    b = 30
+    
+class child1(parent1):
+
+    def calculateSum(self):
+        self.sum = parent1.a + parent1.b
+        print(self.sum)
+
+if __name__ == "__main__":
+    
+    c1 = child1()
+    c1.calculateSum()
+
+
+
+# METHOD 2
+# for accessing instance variable of parent class, parent_class_instance should be passed to child methods as argument.
+
+class parent2:
+
+    def __init__(self):
+        self.a = 20
+        self.b = 30
+    
+class child2(parent2):
+
+    def calculateSum(self, p2):
+        self.sum = p2.a + p2.b
+        print(self.sum)
+
+if __name__ == "__main__":
+    
+    c2 = child2()
+    p2 = parent2()
+    c2.calculateSum(p2)
